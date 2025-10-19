957d2c1aa1eb13f7ea254c34d23e7afc86c9b4e3
Muhammed Türkmen <tuerkmen.muhammed@cyber.bka.de>
2025-10-19 20:35:37 +0200

    add Tailwind CSS configuration for styling and theming

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './apps/**/*.{html,ts}',
+    './libs/**/*.{html,ts}',
+    './src/**/*.{html,ts}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

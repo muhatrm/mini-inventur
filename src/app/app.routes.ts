a39c6190299606e6a1417171220585013ec17f8b
Muhammed Türkmen <tuerkmen.muhammed@cyber.bka.de>
2025-10-19 20:36:25 +0200

    add routing configuration for app components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
-import {Routes} from '@angular/router';
+import { Routes } from '@angular/router';
+import { App } from './app';
+import { LoginComponent } from './components/login/login.component';
+import { authGuard } from './guards/auth-guard';
+import {RegisterComponent} from './components/register/register.component';
+import {TermsComponent} from './components/terms/terms.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    title: 'Home',
+    component: App
+  },
+  {
+    path: 'login', title: 'Login', component: LoginComponent
+  },
+  {
+    path: 'register', title: 'Register', component: RegisterComponent
+  },
+  {
+    path: 'terms', title: 'Terms', component: TermsComponent
+  },
+  {
+    path: 'dashboard', title: 'Dashboard', component: DashboardComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+];
